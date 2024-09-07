cff5fd528362940ca41ce1c0a6e2048aa4aed585
SayanSarkar7 <sarkarsayan602@gmail.com>
2024-09-07 12:25:35 +0530

    final commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechStar</h1>
+            <p>@all rights reserved</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https://github.com/SayanSarkar7" target={"blank"}>GitHub</a>
+                <a href="https://www.youtube.com/@sayan_sarkar_7" target={"blank"}>YouTube</a>
+                <a href="https://www.instagram.com/sayan_sarkar_7/" target={"blank"}>Instagram</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
